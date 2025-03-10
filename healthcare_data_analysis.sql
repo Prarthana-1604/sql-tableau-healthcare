bc6cfa80a468729ad56aa3f4421d00ed4b65161b
Prarthana-1604 <prarthanashetty345@gmail.com>
2025-03-10 18:40:45 +0530

    Update healthcare_data_analysis.sql

--- healthcare_data_analysis.sql
+++ healthcare_data_analysis.sql
@@ -39,3 +39,51 @@ GROUP BY Gender
 SELECT Name,DATEDIFF(DAY, Date_of_Admission, Discharge_Date) AS Stay_Duration
 FROM healthcare_dataset
 ORDER BY Stay_Duration DESC;
+
+--Find the Average Billing Amount by Gender
+SELECT Gender, AVG(Billing_Amount) AS Avg_Billing
+FROM healthcare_dataset
+GROUP BY Gender;
+
+--Identifying the most Common Diagnosis
+SELECT TOP 5 Medical_Condition, COUNT(*) AS Frequency
+FROM healthcare_dataset
+GROUP BY Medical_Condition
+ORDER BY Frequency DESC;
+
+--Categorizing Patients by Age Groups
+SELECT 
+    CASE 
+        WHEN Age BETWEEN 0 AND 18 THEN 'Children (0-18)'
+        WHEN Age BETWEEN 19 AND 40 THEN 'Young Adults (19-40)'
+        WHEN Age BETWEEN 41 AND 60 THEN 'Middle Age (41-60)'
+        ELSE 'Senior Citizens (60+)'
+    END AS Age_Group,
+    COUNT(*) AS Patient_Count
+FROM healthcare_dataset
+GROUP BY 
+    CASE 
+        WHEN Age BETWEEN 0 AND 18 THEN 'Children (0-18)'
+        WHEN Age BETWEEN 19 AND 40 THEN 'Young Adults (19-40)'
+        WHEN Age BETWEEN 41 AND 60 THEN 'Middle Age (41-60)'
+        ELSE 'Senior Citizens (60+)' 
+    END
+ORDER BY Patient_Count DESC;
+
+--Hospital with Highest Average Billing
+SELECT TOP 1 Hospital, AVG(Billing_Amount) AS Avg_Billing
+FROM healthcare_dataset
+GROUP BY Hospital
+ORDER BY Avg_Billing DESC;
+
+--Finding the month with most admissions
+SELECT MONTH(Date_of_Admission) AS Month, COUNT(*) AS Total_Admissions
+FROM healthcare_dataset
+GROUP BY MONTH(Date_of_Admission)
+ORDER BY Total_Admissions DESC;
+
+--Finding patients who stayed more than 10 days
+SELECT Name, Hospital, DATEDIFF(DAY, Date_of_Admission, Discharge_Date) AS Stay_Duration
+FROM healthcare_dataset
+WHERE DATEDIFF(DAY, Date_of_Admission, Discharge_Date) > 10
+ORDER BY Stay_Duration DESC;
